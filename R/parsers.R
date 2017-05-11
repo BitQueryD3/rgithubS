0ebfb81368e1c52a566e932efc509bde72f038e4
Lukas Borke <admin@quantnet.technology>
2017-05-11 08:10:56 +0200

    update package: add full search and 4 parsers

--- R/parsers.R
+++ R/parsers.R
@@ -6,32 +6,81 @@
 #' q_search = 'Quantlet Published Description Keywords Author filename:"metainfo.txt"'
 #' spec_search_term = "yaml user:Quantlet user:lborke user:b2net"
 #' sr = search.code(paste(spec_search_term, q_search), per_page = 20)	
-#' q_top = yaml.parser.light(sr, print_item = FALSE)
+#' q_top = yaml.search.parser(sr, print_item = FALSE)
 #' 
 #' # search for Gitenberg ebooks
 #' r_pack_search = 'gitenberg filename:"metadata.yaml"'
 #' spec_search_term = "king arthur"
 #' sr = search.code(paste(spec_search_term, r_pack_search), per_page = 20)
-#' yaml_top = yaml.parser.light(sr, print_item = FALSE)	
+#' yaml_top = yaml.search.parser(sr, print_item = FALSE)	
 #' }
 #' @export
-yaml.parser.light = function(search.code.results, print_item = TRUE, error_to_results = TRUE) {
+yaml.search.parser = function(search.code.results, print_item = FALSE, error_to_results = TRUE) {
 	yaml_list = list()
-	for (item in search.code.results$content$items) {
+	techinfo = list()
+	error_count = 0
+	i_counter = 0
+	
+	if (!is.null(search.code.results$content$items)) {
+		items_list = search.code.results$content$items
+	} else if (!is.null(search.code.results$full_search)) {
+		items_list = search.code.results$full_search
+	} else {
+		items_list = search.code.results
+	}
+	
+	item_len = length(items_list)
+	
+	for (item in items_list) {
 		path_raw = paste("https://raw.githubusercontent.com", item$repository$full_name, "master", item$path, sep ="/")
 		r = GET(path_raw)
 		q_str = httr::content(r, "text")
 		result = try( meta_parsed_yaml <- yaml.load(q_str), silent = FALSE )
-		if (class(result) == "try-error") {
+		if ("try-error" %in% class(result)) {
 			print(path_raw); flush.console()
-			if (error_to_results) {yaml_list = c(yaml_list, list("YAML error"))}
+			error_count = error_count + 1
+			if (error_to_results) {
+				parser_res = list()
+				parser_res$ok		= FALSE
+				parser_res$path		= item$repository$full_name
+				parser_res$score	= item$score
+				# yaml_list = c(yaml_list, list("YAML error"))
+				yaml_list	= c(yaml_list, list(meta_parsed_yaml))
+				techinfo	= c(techinfo, list(parser_res))
+			}
 		} else {
 			if (print_item) {print(meta_parsed_yaml); flush.console()}
-			yaml_list = c(yaml_list, list(meta_parsed_yaml))
+			parser_res = list()
+			parser_res$ok		= TRUE
+			parser_res$path		= item$repository$full_name
+			parser_res$score	= item$score
+			yaml_list	= c(yaml_list, list(meta_parsed_yaml))
+			techinfo	= c(techinfo, list(parser_res))
+		}
+		i_counter = i_counter + 1
+		if ((i_counter %% 10) == 0) {
+			print(paste("item number:", i_counter))
+			print(paste(round(i_counter / item_len, 4) * 100, "% done"))
+			flush.console()
 		}
 	}
-	return(yaml_list)
+	
+	ok_vec		= sapply( techinfo, function(info){ info$ok } )
+	path_vec	= sapply( techinfo, function(info){ info$path } )
+	score_vec	= sapply( techinfo, function(info){ info$score } )
+	
+	res = list()
+	res$parsed_list	= yaml_list
+	res$techinfo	= techinfo
+	res$error_count = error_count
+	res$type		= "yaml"
+	res$ok_vec		= ok_vec
+	res$path_vec	= path_vec
+	res$score_vec	= score_vec
+	
+	return(res)
 }
+
 
 
 #' Parse DCF encoded metadata retrieved via \code{search.code}
@@ -43,66 +92,202 @@ yaml.parser.light = function(search.code.results, print_item = TRUE, error_to_re
 #' 				filename:"DESCRIPTION" path:"/"'
 #' spec_search_term = "yaml"
 #' sr = search.code(paste(spec_search_term, r_pack_search), per_page = 10)
-#' dcf_top = dcf.parser.light(sr)
+#' dcf_top = dcf.search.parser(sr)
 #' }
 #' @export
-dcf.parser.light = function(search.code.results, print_item = TRUE, error_to_results = TRUE) {
+dcf.search.parser = function(search.code.results, print_item = FALSE, error_to_results = TRUE) {
 	dcf_list = list()
-	for (item in search.code.results$content$items) {
+	techinfo = list()
+	error_count = 0
+	i_counter = 0
+	
+	if (!is.null(search.code.results$content$items)) {
+		items_list = search.code.results$content$items
+	} else if (!is.null(search.code.results$full_search)) {
+		items_list = search.code.results$full_search
+	} else {
+		items_list = search.code.results
+	}
+	
+	item_len = length(items_list)
+	
+	for (item in items_list) {
 		path_raw_dcf = paste("https://raw.githubusercontent.com", item$repository$full_name, "master/DESCRIPTION", sep ="/")
 		con = url(path_raw_dcf)
 		result = try( meta_parsed_cran <- read.dcf(con, all = TRUE) )
 		close(con)
-		if (class(result) == "try-error") {
+		if ("try-error" %in% class(result)) {
 			print(path_raw_dcf); flush.console()
-			if (error_to_results) {dcf_list = c(dcf_list, NULL)}
+			error_count = error_count + 1
+			if (error_to_results) {
+				parser_res = list()
+				parser_res$ok		= FALSE
+				parser_res$path		= item$repository$full_name
+				parser_res$score	= item$score
+				# dcf_list = c(dcf_list, list("DCF error"))
+				dcf_list	= c(dcf_list, list(meta_parsed_cran))
+				techinfo	= c(techinfo, list(parser_res))
+			}
 		} else {
 			if (print_item) {print(meta_parsed_cran); flush.console()}
-			dcf_list = c(dcf_list, list(meta_parsed_cran))
+			parser_res = list()
+			parser_res$ok		= TRUE
+			parser_res$path		= item$repository$full_name
+			parser_res$score	= item$score
+			dcf_list	= c(dcf_list, list(meta_parsed_cran))
+			techinfo	= c(techinfo, list(parser_res))
+		}
+		i_counter = i_counter + 1
+		if ((i_counter %% 10) == 0) {
+			print(paste("item number:", i_counter))
+			print(paste(round(i_counter / item_len, 4) * 100, "% done"))
+			flush.console()
 		}
 	}
-	return(dcf_list)
+	
+	ok_vec		= sapply( techinfo, function(info){ info$ok } )
+	path_vec	= sapply( techinfo, function(info){ info$path } )
+	score_vec	= sapply( techinfo, function(info){ info$score } )
+		
+	res = list()
+	res$parsed_list	= dcf_list
+	res$techinfo	= techinfo
+	res$error_count = error_count
+	res$type		= "CRAN/dcf"
+	res$ok_vec		= ok_vec
+	res$path_vec	= path_vec
+	res$score_vec	= score_vec
+	
+	return(res)
 }
 
-json.parser.light = function(search.code.results, print_item = TRUE, error_to_results = TRUE) {
+
+
+json.search.parser = function(search.code.results, print_item = FALSE, error_to_results = TRUE) {
 	json_list = list()
-	for (item in search.code.results$content$items) {
+	techinfo = list()
+	error_count = 0
+	i_counter = 0
+
+	if (!is.null(search.code.results$content$items)) {
+		items_list = search.code.results$content$items
+	} else if (!is.null(search.code.results$full_search)) {
+		items_list = search.code.results$full_search
+	} else {
+		items_list = search.code.results
+	}
+	
+	item_len = length(items_list)
+	
+	for (item in items_list) {
 		#path_raw = paste("https://raw.githubusercontent.com", item$repository$full_name, "master", item$path, sep ="/")
 		path_raw = gsub("github.com", "raw.githubusercontent.com", item$html_url)
 		path_raw = gsub("/blob", "", path_raw)
 		r = GET(path_raw)
 		q_str = httr::content(r, "text")
 		result = try( meta_parsed_json <- fromJSON(q_str), silent = FALSE )
-		if (class(result) == "try-error") {
+		# if (class(result) == "try-error") {
+		if ("try-error" %in% class(result)) {
 			print(path_raw); flush.console()
-			if (error_to_results) {json_list = c(json_list, list("JSON error"))}
+			error_count = error_count + 1
+			if (error_to_results) {
+				parser_res = list()
+				parser_res$ok		= FALSE
+				parser_res$path		= item$repository$full_name
+				parser_res$score	= item$score
+				# res$error = "JSON error"
+				json_list	= c(json_list, list(meta_parsed_json))
+				techinfo	= c(techinfo, list(parser_res))
+			}
 		} else {
 			if (print_item) {print(meta_parsed_json); flush.console()}
-			meta_parsed_json$path = item$repository$full_name
-			meta_parsed_json$score = item$score
-			json_list = c(json_list, list(meta_parsed_json))
+			
+			parser_res = list()
+			parser_res$ok		= TRUE
+			parser_res$path		= item$repository$full_name
+			parser_res$score	= item$score
+			
+			json_list	= c(json_list, list(meta_parsed_json))
+			techinfo	= c(techinfo, list(parser_res))
 		}
-	} 
-	return(json_list)
-}
-
-get.field = function(f_name, object) {
-	field = sapply(object, function(item){ if (is.recursive(item)) {item[f_name]} else {"error"} } ) # if JSON is not valid
-	if (f_name=="score") {n_field = round(as.numeric(field), 2)}
-	else {
-		field = unname(field)
-		n_field = sapply(field, function(item){paste(unlist(item), collapse=", ")} ) # if data field does not exist or there is a vector of values
-    	n_field = unlist(n_field)
-    }
-	return(n_field)
-}
-
-get.data = function(fields, object) {
-	dat = data.frame(matrix(, nrow=length(object), ncol=0))
-	for (item in fields) {
-		col = get.field(item, object)
-		dat = cbind(dat, col)
+		i_counter = i_counter + 1
+		if ((i_counter %% 10) == 0) {
+			print(paste("item number:", i_counter))
+			print(paste(round(i_counter / item_len, 4) * 100, "% done"))
+			flush.console()
+		}
 	}
-	colnames(dat) <- paste("package.", fields, sep="")
-	return(dat)
+	
+	ok_vec		= sapply( techinfo, function(info){ info$ok } )
+	path_vec	= sapply( techinfo, function(info){ info$path } )
+	score_vec	= sapply( techinfo, function(info){ info$score } )
+	
+	res = list()
+	res$parsed_list	= json_list
+	res$techinfo	= techinfo
+	res$error_count = error_count
+	res$type		= "json"
+	res$ok_vec		= ok_vec
+	res$path_vec	= path_vec
+	res$score_vec	= score_vec
+	
+	return(res)
 }
+
+
+
+flat.search.parser = function(search.code.results, print_item = TRUE, error_to_results = TRUE) {
+	flat.text_list = list()
+	techinfo = list()
+	i_counter = 0
+	
+	if (!is.null(search.code.results$content$items)) {
+		items_list = search.code.results$content$items
+	} else if (!is.null(search.code.results$full_search)) {
+		items_list = search.code.results$full_search
+	} else {
+		items_list = search.code.results
+	}
+	
+	item_len = length(items_list)	
+	
+	for (item in items_list) {
+		path_raw = gsub("github.com", "raw.githubusercontent.com", item$html_url)
+		path_raw = gsub("/blob", "", path_raw)
+		r = GET(path_raw)
+		q_str = httr::content(r, "text")
+		
+		if (print_item) {print(q_str); flush.console()}
+		
+		parser_res = list()
+		parser_res$ok		= TRUE
+		parser_res$path		= item$repository$full_name
+		parser_res$score	= item$score
+		flat.text_list	= c(flat.text_list, list(q_str))
+		techinfo		= c(techinfo, list(parser_res))
+		
+		i_counter = i_counter + 1
+		if ((i_counter %% 10) == 0) {
+			print(paste("item number:", i_counter))
+			print(paste(round(i_counter / item_len, 4) * 100, "% done"))
+			flush.console()
+		}
+	}
+	
+	ok_vec		= sapply( techinfo, function(info){ info$ok } )
+	path_vec	= sapply( techinfo, function(info){ info$path } )
+	score_vec	= sapply( techinfo, function(info){ info$score } )
+	
+	res = list()
+	res$parsed_list	= flat.text_list
+	res$techinfo	= techinfo
+	res$type		= "plain text"
+	res$ok_vec		= ok_vec
+	res$path_vec	= path_vec
+	res$score_vec	= score_vec
+	
+	return(res)
+}
+
+
+
--- R/search.R
+++ R/search.R
@@ -41,3 +41,89 @@ search.users <- function(q, ..., ctx = get.github.context()) {
   params$q <- q
   .api.get.request(ctx, c("search", "users"), params=params, config=add_headers(Accept="application/vnd.github.preview+json"))
 }
+
+
+# New Search API functions
+
+search.code.full = function(query_str, delay_l = 2, max_items = 1000, print_stats = TRUE) {
+
+	if ( (max_items <= 0) | max_items %% 1 !=0 ) {stop("'max_items' must be a positive integer!")}
+	
+	sr = search.code(query_str, per_page = min(100, max_items))
+	Sys.sleep(delay_l)
+	
+	items_total_count	= sr$content$total_count
+	incomplete_results	= sr$content$incomplete_results
+	rate_limit			= sr$headers[["x-ratelimit-remaining"]]
+	
+	print(query_str)
+	print(paste("items_total_count:", items_total_count))
+	print(paste("incomplete_results:", incomplete_results))
+	print(paste("initial Rate limit::", rate_limit))
+	flush.console()
+	
+	full_search = sr$content$items
+	
+	# makes sure that the loop terminates always after 1000 items, the Search API does not provide more
+	max_items = min(max_items, 1000)
+	iter_total_count = min(items_total_count, max_items)
+	max_iter = ceiling(iter_total_count / 100)
+	# calculate the remaining items for the last loop
+	last_iter_rest = iter_total_count %% 100
+	if (last_iter_rest == 0) {last_iter_rest = 100}
+	
+	if (max_iter > 1) {
+		for (i in 2:max_iter){
+			if (i == max_iter) { current_page_size = last_iter_rest } else { current_page_size = 100 }
+			sr = search.code(query_str, per_page = current_page_size, page = i)
+			Sys.sleep(delay_l)
+			while (!sr$ok) {
+				# try again if the api call not successful
+				sr = search.code(query_str, per_page = current_page_size, page = i)
+				print(paste("tried again, page:", i)); flush.console()
+				Sys.sleep(delay_l)
+			}
+			full_search = c(full_search, sr$content$items)
+			
+			print(paste("length of the retrieved items:", length(full_search)))
+			print(paste("Rate limit:", sr$headers[["x-ratelimit-remaining"]]))
+			flush.console()
+			while (as.numeric(sr$headers[["x-ratelimit-remaining"]]) < 5) {
+				Sys.sleep(delay_l)
+				print(paste("Waiting mode; rate limit:", sr$headers[["x-ratelimit-remaining"]]))
+				flush.console()
+			}
+		}
+	}
+		
+	( repo_desc	= sapply( full_search, function(item){ if (is.null(item$repository$description)) { "" } else {item$repository$description} } ))
+	( repo_name	= sapply( full_search, function(item){ item$repository$name } ) )
+	( gh_path	= sapply( full_search, function(item){ item$repository$full_name } ) )
+	( gh_login	= sapply( full_search, function(item){ item$repository$owner$login } ) )
+	( filepath	= sapply( full_search, function(item){ item$path } ) )
+	( scores	= sapply( full_search, function(item){ item$score} ) )
+
+	if (print_stats) {
+		print(sort(table(repo_desc), decreasing = T))
+		print(sort(table(repo_name), decreasing = T))
+		print(sort(table(gh_path), decreasing = T))
+		print(sort(table(gh_login), decreasing = T))
+		print(sort(table(filepath), decreasing = T))
+		boxplot(scores)
+	}
+	
+	print("-------------------------")
+	print(paste("Total number of the retrieved items:", length(full_search)))
+	
+	res = list()
+	res$repo_desc	= repo_desc
+	res$repo_name	= repo_name
+	res$gh_path		= gh_path
+	res$gh_login	= gh_login
+	res$filepath	= filepath
+	res$scores		= scores
+	
+	res$full_search	= full_search
+	
+	return(res)
+}
